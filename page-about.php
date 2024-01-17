d33c4d70ebe16e3ca654d6083a49b30b000c1e18
habanerocity <lindyo87@gmail.com>
2024-01-17 22:41:34 +0800

    added overlay effect to about page

--- page-about.php
+++ page-about.php
@@ -1,24 +1,25 @@
 <?php get_header(); ?>
     <?php include 'custom-header.php'?>
-        <div id="content" class="site-content">
-            <div id="primary" class="content-area">
-                <div id="main" class="site-main">
-                    <div class="container">
-                        <div class="general-template">
-                            <?php 
-                                if( have_posts() ):
-                                    while( have_posts() ) : the_post();
-                                    ?>
-                                        <article>
-                                            <h1><?php the_title(); ?></h1>
-                                            <?php the_content(); ?>
-                                        </article>
-                                    <?php
-                                    endwhile;
-                                else: ?>
-                                    <p>No posts to be displayed!</p>
-                            <?php endif; ?>
-                        </div>
+        <div class="overlay hidden"></div>
+            <div id="content" class="site-content">
+                <div id="primary" class="content-area">
+                    <div id="main" class="site-main">
+                        <div class="container">
+                            <div class="general-template">
+                                <?php 
+                                    if( have_posts() ):
+                                        while( have_posts() ) : the_post();
+                                        ?>
+                                            <article>
+                                                <h1><?php the_title(); ?></h1>
+                                                <?php the_content(); ?>
+                                            </article>
+                                        <?php
+                                        endwhile;
+                                    else: ?>
+                                        <p>No posts to be displayed!</p>
+                                <?php endif; ?>
+                         </div>
                     </div>
                 </div>
             </div>
