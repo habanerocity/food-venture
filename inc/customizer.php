9ba8f1baae340fe6915e9c3e861fb9ea964402bb
habanerocity <lindyo87@gmail.com>
2024-01-19 07:38:44 +0800

    finished about page structure

--- inc/customizer.php
+++ inc/customizer.php
@@ -5,8 +5,8 @@ function wp_foodventure_customizer($wp_customize){
     $wp_customize->add_section(
         'sec_about',
         array(
-            'title' => 'About Section',
-            'description' => 'About Description'
+            'title' => 'About Page',
+            'description' => 'Edit the About Page'
         )
     );
 
@@ -30,14 +30,52 @@ function wp_foodventure_customizer($wp_customize){
         )
     );
 
-    //About Section Image
-    $wp_customize->add_section(
-        'sec_about_image',
+    //About Our Mission Title
+    $wp_customize->add_setting(
+        'set_our_mission_title',
         array(
-            'title' => 'About Image',
+            'type' => 'theme_mod',
+            'default' => 'Our Mission',
+            'sanitize_callback' => 'wp_filter_nohtml_kses'
         )
     );
 
+    $wp_customize->add_control(
+        'set_our_mission_title',
+        array(
+            'label' => 'Our Mission Title',
+            'description' => 'Enter a heading for the Our Mission section',
+            'section' => 'sec_about',
+            'type' => 'text'
+        )
+    );
+
+    //About Our Mission Description
+    $wp_customize->add_setting(
+        'set_our_mission_description',
+        array(
+            'type' => 'theme_mod',
+            'default' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna 
+            aliqua. Sit amet purus gravida quis blandit turpis cursus 
+            in. Odio facilisis mauris sit amet massa vitae tortor 
+            condimentum lacinia. Pharetra et ultrices neque ornare 
+            aenean euismod elementum.',
+            'sanitize_callback' => 'sanitize_textarea_field'
+        )
+    );
+
+    $wp_customize->add_control(
+        'set_our_mission_description',
+        array(
+            'label' => 'Our Mission Description',
+            'description' => 'Enter a description for the Our Mission section',
+            'section' => 'sec_about',
+            'type' => 'textarea'
+        )
+    );
+
+    //About Section Image
     $wp_customize->add_setting(
         'set_about_image',
         array(
@@ -52,7 +90,7 @@ function wp_foodventure_customizer($wp_customize){
             'set_about_image',
             array(
                 'label' => 'About Image',
-                'section' => 'sec_about_image',
+                'section' => 'sec_about',
                 'mime_type' => 'image'
             )
             ));
--- page-about.php
+++ page-about.php
@@ -9,11 +9,27 @@
                     ?>
                     <div class="general-template">
                         <div class="container">
-                            <h3><?php echo get_theme_mod( 'set_description', 'Hi, we are foodies interesting in exploring the sights and flavors of the world, one bite at a time. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec sagittis aliquam malesuada bibendum arcu vitae elementum.' ); ?></h3>
+                            <h3 class="margin-section"><?php echo get_theme_mod( 'set_description', 'Hi, we are foodies interesting in exploring the sights and flavors of the world, one bite at a time. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec sagittis aliquam malesuada bibendum arcu vitae elementum.' ); ?></h3>
                         </div>
-                        <div class="flex_container-row">
+                        <div class="flex_container-row border-top">
                             <div class="container">
-                                <img src="<?php echo $about_image ?>" alt="" style="max-width: 40rem;" />
+                                <div class="flex_container-row padding-section">
+                                    <div class="about_us_pic-wrapper">
+                                        <img src="<?php echo $about_image ?>" alt="Our Picture" class="about-pic" />
+                                    </div>
+                                    <div class="our_mission-wrapper">
+                                        <h2 class="mb-0"><?php echo get_theme_mod( 'set_our_mission_title', 'Our Mission' ); ?></h2>
+                                        <p class="our_mission-description"><?php echo get_theme_mod('set_our_mission_description', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet purus gravida quis blandit turpis cursus in. Odio facilisis mauris sit amet massa vitae tortor condimentum lacinia. Pharetra et ultrices neque ornare aenean euismod elementum.') ?></p>
+                                        <div class="our_mission-btn_wrapper">
+                                        <a class="unstyled-link" href="<?php echo get_permalink(get_page_by_title('Contact')); ?>">
+                                            <button class="btn_round-transparent margin_right-xl">Contact</button>
+                                        </a>
+                                        <a class="unstyled-link" href="<?php echo get_permalink(get_page_by_title('Blog')) ?>">
+                                            <button class="btn_round-solid">View Blog</button>
+                                        </a>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
