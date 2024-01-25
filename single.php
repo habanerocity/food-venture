1621ea6213acfb0289c673d9682a0dbaef983f8b
habanerocity <lindyo87@gmail.com>
2024-01-26 00:35:20 +0800

    added comment section to blog articles

--- single.php
+++ single.php
@@ -24,20 +24,37 @@
                         
                     </article>
                     <?php get_sidebar(); ?>
-                    <?php
-                endwhile;
-                ?>
+                    
             </div>
-            <div class="post-navigation">
-                <div class="container padding-section" style="display: flex; justify-content: space-between;">
-                    <div class="prev-post">
-                        <?php previous_post_link('%link', 'Previous Post', TRUE); ?>
-                    </div>
-                    <div class="next-post">
-                        <?php next_post_link('%link', 'Next Post', TRUE); ?>
+            <?php
+            $prev_link = get_previous_post_link('%link', 'Previous Post', TRUE);
+            $next_link = get_next_post_link('%link', 'Next Post', TRUE);
+
+            if ($prev_link || $next_link) : ?>
+                <div class="post-navigation">
+                    <div class="container padding-section" style="display: flex; justify-content: space-between;">
+                        <div class="prev-post">
+                            <?php echo $prev_link; ?>
+                        </div>
+                        <div class="next-post">
+                            <?php echo $next_link; ?>
+                        </div>
                     </div>
                 </div>
-            </div>
+            <?php endif; ?>
+            <?php
+            if (comments_open() || get_comments_number()) {
+                echo '<div class="comment__section-container">';
+                echo '<div class="container padding-section">';
+                comments_template();
+                echo '</div>';
+                echo '</div>';
+            }
+            
+            ?>
+            <?php
+                endwhile;
+                ?>
         </div>
     </div>
 <?php get_footer(); ?>
