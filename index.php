7e3a24d7cc9f023d1ec5955ae0cf310e0078bbf0
habanerocity <lindyo87@gmail.com>
2024-03-06 02:34:53 +0800

    improved index page security

--- index.php
+++ index.php
@@ -26,9 +26,11 @@
                                             'selected'          => $selected_option, // Set the selected option
                                         ) );
 
+                                        $home_url = esc_url(get_home_url());
+
                                         if($categories){
                                             // Add onchange event to the dropdown
-                                            $categories = str_replace('<select', '<select onchange="if(this.value && this.value != \'-1\') { window.location.href=\''.get_home_url().'/category/\'+this.value; } else if(this.value == \'\') { window.location.href=\''.get_home_url().'/\' }"', $categories);
+                                            $categories = str_replace('<select', '<select onchange="if(this.value && this.value != \'-1\') { window.location.href=\''.$home_url.'/category/\'+this.value; } else if(this.value == \'\') { window.location.href=\''.$home_url.'/\' }"', $categories);
                                             echo $categories;
                                         }
 
@@ -75,20 +77,20 @@
                                                 <article class="article__card-index">
                                                     <div class="article__card-top_wrapper">
                                                         <div class="featured-thumbnail">
-                                                            <a href="<?php the_permalink(); ?>">
-                                                                <?php the_post_thumbnail('large', ['class' => 'article__card-img']); ?>
+                                                            <a href="<?php echo esc_url(get_the_permalink()); ?>">
+                                                                <img src="<?php echo esc_url(get_the_post_thumbnail_url(null, 'large')); ?>" class="article__card-img" alt="">
                                                             </a>
                                                         </div>
-                                                        <a href="<?php the_permalink(); ?>">
+                                                        <a href="<?php echo esc_url(get_the_permalink()); ?>">
                                                             <h4 class="article__card-heading">
-                                                                <?php the_title(); ?>
+                                                                <?php echo esc_html(the_title()); ?>
                                                             </h4>
                                                         </a>
                                                         <?php the_excerpt(); ?>
                                                     </div>
                                                     <div class="article__card-index-footer">
-                                                        <span class="article__card-index-footer_date"><i class="fas fa-calendar-alt"></i> <?php echo get_the_date(); ?></span>
-                                                        <span class="article__card-index-footer_link"><a href="<?php the_permalink(); ?>">Read More</a></span>
+                                                        <span class="article__card-index-footer_date"><i class="fas fa-calendar-alt"></i> <?php echo esc_html(get_the_date()); ?></span>
+                                                        <span class="article__card-index-footer_link"><a href="<?php echo esc_url(the_permalink()); ?>">Read More</a></span>
                                                     </div>
                                                 </article>
                                             <?php
