11dd69253c3c91b9698d003d552ccb6a31615933
habanerocity <lindyo87@gmail.com>
2024-03-09 01:49:21 +0800

    made latest-recipes html more semantic

--- parts/content-latest-recipes.php
+++ parts/content-latest-recipes.php
@@ -7,16 +7,16 @@ if (!empty($image_url)) {
     $heading = get_post_meta(get_the_ID(), 'recipe_thumbnail-heading', true);
     $subheading = get_post_meta(get_the_ID(), 'recipe_thumbnail-subheading', true);
 
-    echo '<div class="latest__recipes-thumbnail_container">';
+    echo '<article class="latest__recipes-thumbnail_container">';
     echo '<a href="' . esc_url(get_permalink()) . '">';
     echo '<img class="latest__recipes-thumbnail" src="' . esc_url($image_url) . '">';
     echo '<div class="latest__recipes-thumbnail_overlay">';
-    echo '<div class="latest__recipes-thumbnail_headings">';
+    echo '<header class="latest__recipes-thumbnail_headings">';
     echo '<h4 class="latest__recipes-thumbnail_title">' . esc_html($heading) . '</h4>';
     echo '<h5 class="latest__recipes-thumbnail_subtitle">' . esc_html($subheading) . '</h5>';
-    echo '</div>';
+    echo '</header>';
     echo '</div>';
     echo '</a>';
-    echo '</div>';
+    echo '</article>';
 }
 ?>
