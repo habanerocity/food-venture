ff44354109bed7bd66aa18c13544d46326bca31e
habanerocity <lindyo87@gmail.com>
2024-03-11 23:23:34 +0800

    wrapped footer cols in nav tags

--- parts/content-footer-cols.php
+++ parts/content-footer-cols.php
@@ -1,23 +1,31 @@
 <?php if ( is_active_sidebar( 'footer-col-1' ) ) : ?>
     <aside id="footer-col-1" class="footer-column">
-        <?php dynamic_sidebar( 'footer-col-1' ); ?>
+        <nav>
+            <?php dynamic_sidebar( 'footer-col-1' ); ?>
+        </nav>
     </aside>
 <?php endif; ?>
 
 <?php if ( is_active_sidebar( 'footer-col-2' ) ) : ?>
     <aside id="footer-col-2" class="footer-column">
-        <?php dynamic_sidebar( 'footer-col-2' ); ?>
+        <nav>
+            <?php dynamic_sidebar( 'footer-col-2' ); ?>
+        </nav>
     </aside>
 <?php endif; ?>
 
 <?php if ( is_active_sidebar( 'footer-col-3' ) ) : ?>
     <aside id="footer-col-3" class="footer-column">
-        <?php dynamic_sidebar( 'footer-col-3' ); ?>
+        <nav>
+            <?php dynamic_sidebar( 'footer-col-3' ); ?>
+        </nav>
     </aside>
 <?php endif; ?>
 
 <?php if ( is_active_sidebar( 'footer-col-4' ) ) : ?>
     <aside id="footer-col-4" class="footer-column">
-        <?php dynamic_sidebar( 'footer-col-4' ); ?>
+        <nav>
+            <?php dynamic_sidebar( 'footer-col-4' ); ?>
+        </nav>
     </aside>
 <?php endif; ?>
