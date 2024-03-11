9352f2de0898bb3a0430b667ac5d44da58b57299
habanerocity <lindyo87@gmail.com>
2024-03-11 23:55:08 +0800

    made search page more semantic

--- search.php
+++ search.php
@@ -1,28 +1,28 @@
 <?php get_header(); ?>
 <div class="overlay hidden"></div>
 <?php get_template_part('parts/content', 'custom-header') ?>
-    <div id="content" class="site-content">
-        <div id="primary" class="content-area">
-            <main id="main" class="site-main">
-                <div class="container padding-section blog__container">
-                    <div class="blog__articles">
-                        <div class="blog__items-grid">
-                            <?php 
-                                if( have_posts() ):
-                                    while( have_posts() ) : the_post();
-                                    ?>
-                                        <?php if('post' == get_post_type() || 'blog_recipes' == get_post_type()): ?>
-                                        <?php get_template_part( 'parts/content', 'article_card-index'); ?>
-                                        <?php endif; ?>
-                                    <?php
-                                    endwhile;
-                                else: ?>
-                                    <p>No posts to be displayed!</p>
-                            <?php endif; ?>
-                        </div>
+<main id="content" class="site-content">
+    <section id="primary" class="content-area">
+        <div id="main" class="site-main">
+            <div class="container padding-section blog__container">
+                <article class="blog__articles">
+                    <div class="blog__items-grid">
+                        <?php 
+                            if( have_posts() ):
+                                while( have_posts() ) : the_post();
+                                ?>
+                                    <?php if('post' == get_post_type() || 'blog_recipes' == get_post_type()): ?>
+                                    <?php get_template_part( 'parts/content', 'article_card-index'); ?>
+                                    <?php endif; ?>
+                                <?php
+                                endwhile;
+                            else: ?>
+                                <p>No posts to be displayed!</p>
+                        <?php endif; ?>
                     </div>
-                </div>
-            </main>
+                </article>
+            </div>
         </div>
-    </div>
+    </section>
+</main>
 <?php get_footer(); ?>
