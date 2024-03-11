914c3f9de30ccc09a508cb67cfa5e1a3057610ef
habanerocity <lindyo87@gmail.com>
2024-03-11 23:50:48 +0800

    made contact page more semantic

--- page-contact.php
+++ page-contact.php
@@ -1,23 +1,23 @@
 <?php get_header(); ?>
 <?php get_template_part('parts/content', 'custom-header') ?>
-    <div class="overlay hidden"></div>
-    <div id="content" class="site-content">
-        <div id="primary" class="content-area">
-            <div id="main" class="site-main">
-                <?php
-                $contact_image = wp_get_attachment_url(get_theme_mod('set_contact_image'));
-                ?>
-                <div class="container">
-                    <div class="contact__form-container contact__row padding-section">
-                        <div class="contact__wrapper ">
-                            <img class="contact__pic" src="<?php echo esc_url($contact_image)  ?>" />
-                        </div>
-                        <div class="contact__form-wrapper">
-                            <?php echo do_shortcode('[contact-form-7 id="57e1d9f" title="Contact form"]'); ?>
-                        </div>
+<div class="overlay hidden"></div>
+<main id="content" class="site-content">
+    <section id="primary" class="content-area">
+        <div id="main" class="site-main">
+            <?php
+            $contact_image = wp_get_attachment_url(get_theme_mod('set_contact_image'));
+            ?>
+            <div class="container">
+                <section class="contact__form-container contact__row padding-section">
+                    <div class="contact__wrapper ">
+                        <img class="contact__pic" src="<?php echo esc_url($contact_image)  ?>" />
                     </div>
-                </div>
+                    <div class="contact__form-wrapper">
+                        <?php echo do_shortcode('[contact-form-7 id="57e1d9f" title="Contact form"]'); ?>
+                    </div>
+                </section>
             </div>
         </div>
-    </div>
+    </section>
+</main>
 <?php get_footer(); ?>
