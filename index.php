4abcc09ea2e9c2de91e3b22a41cfc33429eec28b
habanerocity <lindyo87@gmail.com>
2024-03-11 23:39:07 +0800

    made index pages more semantic

--- index.php
+++ index.php
@@ -1,28 +1,28 @@
 <?php get_header(); ?>
 <div class="overlay hidden"></div>
 <?php get_template_part('parts/content', 'custom-header') ?>
-        <div id="content" class="site-content">
-            <div id="primary" class="content-area">
-                <main id="main" class="site-main">
-                    <div class="container padding-section blog__container">
-                        <div class="blog__articles">
-                            <div class="blog__input_container">
-                                <?php get_template_part( 'parts/content', 'select-category'); ?>
-                                <?php get_template_part( 'parts/content', 'select-archive'); ?>
-                            </div>
-                            <div class="blog__items-grid">
-                                <?php 
-                                    if( have_posts() ):
-                                        while( have_posts() ) : the_post();
-                                        get_template_part( 'parts/content', 'article_card-index');
-                                        endwhile;
-                                    else: ?>
-                                        <p>No posts to be displayed!</p>
-                                <?php endif; ?>
-                            </div>
-                        </div>
+<main id="content" class="site-content">
+    <section id="primary" class="content-area">
+        <div id="main" class="site-main">
+            <div class="container padding-section blog__container">
+                <header class="blog__articles">
+                    <div class="blog__input_container">
+                        <?php get_template_part( 'parts/content', 'select-category'); ?>
+                        <?php get_template_part( 'parts/content', 'select-archive'); ?>
                     </div>
-                </main>
+                </header>
+                <article class="blog__items-grid">
+                    <?php 
+                        if( have_posts() ):
+                            while( have_posts() ) : the_post();
+                            get_template_part( 'parts/content', 'article_card-index');
+                            endwhile;
+                        else: ?>
+                            <p>No posts to be displayed!</p>
+                    <?php endif; ?>
+                </article>
             </div>
         </div>
+    </section>
+</main>
 <?php get_footer(); ?>
