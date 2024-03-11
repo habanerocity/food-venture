520c0b83ba7531e99b957889ec2f185a95610a60
habanerocity <lindyo87@gmail.com>
2024-03-12 00:42:06 +0800

    added alt tags to home page recipe thumbnails

--- parts/content-hero.php
+++ parts/content-hero.php
@@ -1,10 +1,13 @@
 <?php 
     $args = array( 'numberposts' => '1' );
     $recent_posts = wp_get_recent_posts( $args );
+
     foreach( $recent_posts as $recent ){
         $post_id = $recent["ID"];
         $hero_image = get_field('hero_image', $post_id);
+
         if($hero_image){ // Check if the 'hero_image' field exists
+            $hero_image_alt = $hero_image['alt'];
         ?>
             <article class="hero__post">
                 <section class="hero__post-text_content">
@@ -25,7 +28,7 @@
                 <figure class="hero__post-image_wrapper">
                     <div class="hero__post-image_container">
                         <a href="<?php echo esc_url(get_permalink($post_id)); ?>">
-                            <img src="<?php echo esc_url($hero_image['url']); ?>" alt="" />
+                            <img src="<?php echo esc_url($hero_image['url']); ?>" alt="<?php echo esc_attr($hero_image_alt); ?>" />
                         </a>
                     </div>
                 </figure>
--- parts/content-latest-recipes.php
+++ parts/content-latest-recipes.php
@@ -1,15 +1,18 @@
 <?php
 // Get the image URL
-$image_url = get_field('recipe_thumbnail-home_page');
+$image = get_field('recipe_thumbnail-home_page');
 
 // Render the image
-if (!empty($image_url)) {
+if ($image) {
     $heading = get_post_meta(get_the_ID(), 'recipe_thumbnail-heading', true);
     $subheading = get_post_meta(get_the_ID(), 'recipe_thumbnail-subheading', true);
 
+    $image_url = $image['url'];
+    $image_alt = $image['alt'];
+
     echo '<article class="latest__recipes-thumbnail_container">';
     echo '<a href="' . esc_url(get_permalink()) . '">';
-    echo '<img class="latest__recipes-thumbnail" src="' . esc_url($image_url) . '">';
+    echo '<img class="latest__recipes-thumbnail" alt="' . esc_attr($image_alt) . '" src="' . esc_url($image_url) . '">';
     echo '<div class="latest__recipes-thumbnail_overlay">';
     echo '<header class="latest__recipes-thumbnail_headings">';
     echo '<h4 class="latest__recipes-thumbnail_title">' . esc_html($heading) . '</h4>';
